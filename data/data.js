6f76d492f5fc67da214ca7211f7e87b7efbc1d58
Dawson Myers <Djmyers2002@gmail.com>
2023-04-11 15:42:28 -0400

    initial commit

---
+++ data/data.js
@@ -0,0 +1,9 @@
+const data = [
+    {text: 'Car', fast: true, _id: 125223},
+    {text: 'Turtle', fast: false, _id: 127904},
+    {text: 'Trees', fast: false, _id: 139608},
+  ]
+
+export {
+    data
+}
